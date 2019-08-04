3b21a4624765e023acdcf6775dbff8880623f447
Amanda <amanda.macgregor13@gmail.com>
2019-08-04 04:45:41 +0000

    stopping for the night after some definitions of global vs window

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
+//first thing's first..
+
 function sayHello(name) {
     console.log('Hello ' + name)
 }
 
-// sayHello('Amanda')
-console.log(window)
+sayHello('Amanda')
+// console.log(window) //gives an error
+
+//Modules:
+//OS, FS, Events, HTTP
+
+console.log() //global object. Global scope, can access anywhere
+setTimeout() //also global. Sets a timeout based on timeframe we give it.
+clearTimeout() //also global. 
+
+setInterval() //also global. 
+clearInterval() //also global.  Research these in more depth outside of this tutorial.
+
+// window // JSobject where all above functions can be accessed.
+    // window.console.log = how console.log() translates behind the scenes
+    
+//In Node, we have global instead of window    
+    
